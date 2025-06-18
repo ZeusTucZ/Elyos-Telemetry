acf38da7e5811af29337254b3397bf8708825cdb
ZeusTucZ <lorenzo.orrante@icloud.com>
2025-06-18 13:59:52 -0600

    Conection to the postgresql data base

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-
+import express from 'express';
 const app = express();
 
-app.use(express.json())
-app.use(cors());
+app.use(express.json());
 
-module.exports = app;
+export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
-const app = require('./app');
+import dotenv from 'dotenv';
+import app from './app.js';
+import pool from './databases/dbConfig.js';
 
-require('dotenv').config({ path: './env/.env' });
+// Initiate the enviroment variables
+dotenv.config({ path: './env/.env' });
 
 const PORT = process.env.PORT || 3000;
-console.log(`${PORT}`);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// Try conection before initiating the server
+try {
+  const client = await pool.connect();
+  const res = await client.query("SELECT NOW()");
+  console.log("🟢 Data Base Connected:", res.rows[0]);
+  client.release();
+
+  // Start the server after the conection is succesfull
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+  });
+} catch (err) {
+  console.error("🔴 Error while trying to connect to the data base:", err.stack);
+}
