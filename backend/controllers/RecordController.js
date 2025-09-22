8a3a0d161ac0329d260b4e89101b46f2b499efc5
ZeusTucZ <lorenzo.orrante@icloud.com>
2025-09-22 08:00:04 -0600

    Bring the backend file from backend branch

--- backend/controllers/RecordController.js
+++ backend/controllers/RecordController.js
@@ -8,12 +8,17 @@ export const startRecording = async (req, res) => {
 };
 
 // Stop recording
-export const stopRecording = (req, res) => {
+export const stopRecording = async (req, res) => {
     setIsRunning(false);
     res.json({ message: 'Recording stopped' });
 };
 
 // Status recording
-export const statusRecording = (req, res) => {
+export const statusRecording = async (req, res) => {
     res.json({ isRunning: getIsRunning() });
+};
+
+// Save recording
+export const saveRecording = async (req, res) => {
+    res.json({ message: 'Save recording' });
 };
